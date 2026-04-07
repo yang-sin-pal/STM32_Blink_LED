aa25f46a3d567fa7c2272a1c30a209bf9b8bd840
Duong Tan Phat <phat.duongtan.cit22@eiu.edu.vn>
2026-04-07 22:00:08 +0700

    Refactor rgb_setcolor(), finish basic rgb_blink(). Upgrade struct and Build separate rgb_addColor() & rgb_removeColor() -> rgb_multiToggle() -> rgb_nonblockingBlink() toggle 2 colors simultaneously because HAL_Delay() cannot do this.

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,9 +57,9 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define LED_R_Pin GPIO_PIN_0
+#define LED_R_Pin GPIO_PIN_1
 #define LED_R_GPIO_Port GPIOB
-#define LED_G_Pin GPIO_PIN_1
+#define LED_G_Pin GPIO_PIN_0
 #define LED_G_GPIO_Port GPIOB
 #define LED_B_Pin GPIO_PIN_2
 #define LED_B_GPIO_Port GPIOB
--- User_libs/led.h
+++ User_libs/led.h
@@ -28,7 +28,12 @@ typedef struct
 	uint16_t blue_pin;
 	
 	bool  state;
+	bool red_state;
+	bool green_state;
+	bool blue_state;
 	uint32_t last_time_on;
+	uint32_t tick1;
+	uint32_t tick2;
 	Color color;
 } RGB;
 
@@ -37,6 +42,8 @@ void rgb_setcolor (RGB *rgb, Color color);
 void rgb_toggle(RGB *rgb, Color color);
 void rgb_blink(RGB *rgb, Color color, uint16_t blink_time);
 
-
-
+void rgb_addColor(RGB *rgb, Color color);
+void rgb_removeColor(RGB *rgb, Color color);
+void rgb_multiToggle(RGB *rgb, Color color);
+void rgb_nonBlockingBlink(RGB *rgb, uint16_t blink_time1, uint16_t blink_time2, Color color1, Color color2);
 #endif
